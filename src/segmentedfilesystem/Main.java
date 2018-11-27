dfbd321affbaf723557c2bd9f0c002a4152984c4
lalys <xion1337@morris.umm.edu>
2018-11-27 11:04:06 -0600

    Implement "say hello" packet send in client
    main() sets up a socket on a server specified on command line and sends an empty packet to it.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,26 @@
 package segmentedfilesystem;
 
 public class Main {
-    
+
     public static void main(String[] args) {
+
+        if (args.length != 1) {
+          System.out.println("Please specify a valid server");
+          return;
+        }
+
+        // set up the connection by sending a meaningless packet to server
+        DatagramSocket socket = new DatagramSocket();
+
+        int port = 6014;
+        InetAddress address = InetAddress.getByName(args[0]);
+        byte[] buf = new byte[1000];
+
+        DatagramPacket hello = new DatagramPacket(buf, buf.length, address, port);
+        socket.send(packet);
+
         
+
     }
 
 }
