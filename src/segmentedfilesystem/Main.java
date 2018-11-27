111d347714b7e4b67d330b5d8d8e1a59375e95f1
lalys <xion1337@morris.umm.edu>
2018-11-27 11:22:02 -0600

    Start writing receive packet loop

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -9,17 +9,34 @@ public class Main {
           return;
         }
 
-        // set up the connection by sending a meaningless packet to server
+        // basic server info
         DatagramSocket socket = new DatagramSocket();
-
         int port = 6014;
         InetAddress address = InetAddress.getByName(args[0]);
-        byte[] buf = new byte[1000];
 
+        // set up the connection by sending a meaningless packet to server
+        byte[] buf = new byte[1000];
         DatagramPacket hello = new DatagramPacket(buf, buf.length, address, port);
         socket.send(packet);
 
-        
+        HashMap<Byte, ReceivedFile> receivedFilesById = new HashMap();
+        boolean done = false;
+
+        // start receiving packets
+        while (!done) {
+          // check to see whether all files have been received completely
+          for (ReceivedFile file : receivedFilesById.values()) {
+            if (!file.isComplete()) {
+              done = false;
+              break;
+            }
+            else { done = true; }
+          }
+
+          // receive another packet
+          DatagramPacket packet = new DatagramPacket(buf, buf.length);
+          socket.receive(packet);
+        }
 
     }
 
