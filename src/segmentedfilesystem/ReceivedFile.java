80d7b98f9b57803f96b0757233988bbe97f86d83
lalys <xion1337@morris.umm.edu>
2018-11-29 15:35:08 -0600

    Implement ReceivedFile.isComplete() check

--- src/segmentedfilesystem/ReceivedFile.java
+++ src/segmentedfilesystem/ReceivedFile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ReceivedFile {
     private byte fileId;
     private String filename;
-    private int numPackets;
+    private int numPackets = Integer.MIN_VALUE;
     private ArrayList<DataPacket> data;
     private HeaderPacket header;
 
@@ -23,9 +23,22 @@ public class ReceivedFile {
 
     public void setHeader(HeaderPacket header) {
         this.header = header;
+        this.filename = header.getFilename();
     }
 
     public boolean isComplete() {
-        return false;
+        // if don't know how many packets we're getting yet
+        if (numPackets == Integer.MIN_VALUE) {
+            return false;
+        }
+        // if don't have all packets yet
+        if (this.data.size() != numPackets) {
+            return false;
+        }
+        // if don't have filename from header yet
+        if (filename == null) {
+            return false;
+        }
+        return true;
     }
 }
